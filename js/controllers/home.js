441bdb4deb10021b61ee9e2d71e25680a9234b5b
Rattanak Chea <rattanak.chea@gmail.com>
2016-06-05 00:32:35 -0700

    finish styling part 1

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -6,11 +6,35 @@ app.controller('HomeCtrl', function ($scope, $http) {
 
     var base_url  = 'http://localhost:8000/api';
     
-    $http.get(base_url + '/items').then(function(items){
+    $scope.apiURL = base_url;
+    
+    
+    getItems();
+    
+    function getItems(){
+        $http.get(base_url + '/items').then(function(items){
         
         $scope.items = items.data;
         console.log(items.data);
+    }, function(err){
+        console.log(err);
     });
+    }
     
+    
+    
+    $scope.postItem = function(item){
+        //valid item
+        console.log(item);
+        $http.post(base_url + '/items', item).then(function(msg){
+            console.log(msg.data);
+            
+             getItems();
+            
+            }, function(err){
+            console.log(err);
+        });
+        
+    }
 	
 });
